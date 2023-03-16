b92546742908742794338c613e597e99a79a243a
bitsgalore <johan.vanderknijff@kb.nl>
2023-03-16 16:51:00 +0100

    added rebook retrieval script

---
+++ scripts/pull-ebooks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir dbnl
+mkdir se
+cd dbnl
+wget https://www.dbnl.org/tekst/_bij005198501_01/ebook/_bij005198501_01.epub
+wget https://www.dbnl.org/tekst/_bij005198701_01/ebook/_bij005198701_01.epub
+wget https://www.dbnl.org/tekst/_bij005198901_01/ebook/_bij005198901_01.epub
+wget https://www.dbnl.org/tekst/_bij005199001_01/ebook/_bij005199001_01.epub
+wget https://www.dbnl.org/tekst/_nie264nieu01_01/ebook/_nie264nieu01_01.epub
+wget https://www.dbnl.org/tekst/_bij005199601_01/ebook/_bij005199601_01.epub
+wget https://www.dbnl.org/tekst/duys002lofd01_01/ebook/duys002lofd01_01.epub
+wget https://www.dbnl.org/tekst/g___002ghee01_01/ebook/g___002ghee01_01.epub
+wget https://www.dbnl.org/tekst/rola003meil01_01/ebook/rola003meil01_01.epub
+wget https://www.dbnl.org/tekst/zett001voor01_01/ebook/zett001voor01_01.epub
+cd ..
+cd se
+wget https://standardebooks.org/ebooks/arthur-conan-doyle/the-casebook-of-sherlock-holmes/downloads/arthur-conan-doyle_the-casebook-of-sherlock-holmes.epub
+wget https://standardebooks.org/ebooks/george-macdonald/at-the-back-of-the-north-wind/downloads/george-macdonald_at-the-back-of-the-north-wind.epub
+wget https://standardebooks.org/ebooks/richard-jefferies/amaryllis-at-the-fair/downloads/richard-jefferies_amaryllis-at-the-fair.epub
+wget https://standardebooks.org/ebooks/w-e-b-du-bois/darkwater/downloads/w-e-b-du-bois_darkwater.epub
+wget https://standardebooks.org/ebooks/charles-dickens/dombey-and-son/downloads/charles-dickens_dombey-and-son.epub
+wget https://standardebooks.org/ebooks/g-k-chesterton/the-secret-of-father-brown/downloads/g-k-chesterton_the-secret-of-father-brown.epub
+wget https://standardebooks.org/ebooks/e-nesbit/the-story-of-the-treasure-seekers/downloads/e-nesbit_the-story-of-the-treasure-seekers.epub
+wget https://standardebooks.org/ebooks/anthony-trollope/the-prime-minister/downloads/anthony-trollope_the-prime-minister.epub
+wget https://standardebooks.org/ebooks/andre-norton/key-out-of-time/downloads/andre-norton_key-out-of-time.epub
+wget https://standardebooks.org/ebooks/marie-belloc-lowndes/the-story-of-ivy/downloads/marie-belloc-lowndes_the-story-of-ivy.epub
+cd ..
+
